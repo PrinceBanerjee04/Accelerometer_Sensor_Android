a591f3964f0a7f883b52f41483bbe7650c5d5afc
Prince <banerjee.prince69@gmail.com>
2024-04-06 12:44:14 +0530

    Final Commit

--- app/src/main/java/com/example/accelerometersensor/MainActivity.kt
+++ app/src/main/java/com/example/accelerometersensor/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.accelerometersensor
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sensorManager = getSystemService(SENSOR_SERVICE) as? SensorManager
+        if (sensorManager != null) {
+            val acceleroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+            if (acceleroSensor != null) {
+                sensorManager.registerListener(this, acceleroSensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        } else {
+            Toast.makeText(this, "Sensor service not started", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type==Sensor.TYPE_ACCELEROMETER){
+            findViewById<TextView>(R.id.tv_sensor_values).text = "X: ${event.values[0]}, Y: ${event.values[1]}, Z: ${event.values[2]}"
+        }
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
 }
